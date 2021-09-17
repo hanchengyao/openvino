7342dd4e64430466c62304843efb2d4ea8de39ab
hcyao <99tjjh11808@gmail.com>
2021-09-17 15:32:07 +0800

    remove type_infer function call in prepare_emit_ir

--- model-optimizer/mo/pipeline/common.py
+++ model-optimizer/mo/pipeline/common.py
@@ -191,9 +191,6 @@ def prepare_emit_ir(graph: Graph, data_type: str, output_dir: str, output_model_
 
     for_graph_and_each_sub_graph_recursively(graph, OpVersioning().find_and_replace_pattern)
 
-    # do not run the type inference in sub-graphs. It will be called automatically as part of the type inference of
-    # the TensorIterator nodes
-    type_infer(graph)
     RemoveUselessConvert().find_and_replace_pattern(graph)
 
     ResultRename().find_and_replace_pattern(graph)
